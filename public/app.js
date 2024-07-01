0fc16c8b9844d8caf69255e85a3f0bad292a0bf8
Ryan Connor Christie <26291495@sun.ac.za>
2024-07-01 15:18:58 +0200

    Gyro testing skeleton

---
+++ public/app.js
@@ -0,0 +1,12 @@
+function changeBackground(color) {
+    document.body.style.background = color;
+}
+
+window.addEventListener("load", function () {
+    changeBackground('blue');
+
+    var x = document.getElementById("x");
+    var y = document.getElementById("y");
+    var z = document.getElementById("z");
+
+});
