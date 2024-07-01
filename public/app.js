492edf0e7ce96941a2e1a50dc9f5e8c67b1d2ac4
Ryan Connor Christie <26291495@sun.ac.za>
2024-07-01 16:31:02 +0200

    Fixing issue

--- public/app.js
+++ public/app.js
@@ -2,15 +2,14 @@ window.addEventListener("load", function () {
 
     var y = document.getElementById("y");
     var z = document.getElementById("z");
-    z.innerText = "Testing";
 
     window.addEventListener('deviceorientation', handleOrientation);
 
     function handleOrientation(event) {
         const y_val = event.beta;
         const z_val = event.gamma;
-        y.innerText = Math.round(y_val);
-        z.innerText = Math.round(z_val);
+        y.innerText = "Y: " + Math.round(y_val);
+        z.innerText = "Z: " + Math.round(z_val);
 }
 
 });
