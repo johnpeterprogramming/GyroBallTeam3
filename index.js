100d4c079cad56ba2e4e8285a8106a962abc0ab4
John-Peter <64694222+johnpeterprogramming@users.noreply.github.com>
2024-07-01 14:42:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const port = 3000;
+const port = 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
