0655a66e5a843953e3216162c39ee3a99c556b84
Ryan Connor Christie <26291495@sun.ac.za>
2024-07-02 09:23:37 +0200

    adding check for orientation suppport

--- public/app.js
+++ public/app.js
@@ -3,14 +3,23 @@ window.addEventListener("load", function () {
     var y = document.getElementById("y");
     var z = document.getElementById("z");
 
-    window.addEventListener('deviceorientation', handleOrientation);
 
-    function handleOrientation(event) {
-        const y_val = event.beta;
-        const z_val = event.gamma;
-        y.innerText = "Y: " + Math.round(y_val);
-        z.innerText = "Z: " + Math.round(z_val);
-}
+    
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+
+        window.addEventListener('deviceorientation', handleOrientation);
+
+        function handleOrientation(event) {
+            const y_val = event.beta;
+            const z_val = event.gamma;
+            y.innerText = "Y: " + Math.round(y_val);
+            z.innerText = "Z: " + Math.round(z_val);
+        }
+
+    } else {
+        y.innerText = "Not supported";
+        z.hidden = true;
+    }
 
 });
 
